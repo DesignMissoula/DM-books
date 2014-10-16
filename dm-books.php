fd2b50f927545afbb2a1288bd0641a68f347855f
Bradford Knowlton <brad@ucanbonline.net>
2014-10-15 21:53:00 -0600

    changed to cookbook

--- dm-books.php
+++ dm-books.php
@@ -7,7 +7,7 @@
  * Author: Name Of The Plugin Author
  * Author URI: http://URI_Of_The_Plugin_Author
  * License: A "Slug" license name e.g. GPL2
- * Version: 1.09.3
+ * Version: 1.09.5
  * GitHub Plugin URI: https://github.com/DesignMissoula/DM-books
  * GitHub Branch: master
  */
@@ -15,17 +15,17 @@
      add_action( 'init', 'register_cpt_book' );
     function register_cpt_book() {
     $labels = array(
-    'name' => _x( 'Books', 'book' ),
+    'name' => _x( 'CookBooks', 'book' ),
     'singular_name' => _x( 'book', 'book' ),
     'add_new' => _x( 'Add New', 'book' ),
     'add_new_item' => _x( 'Add New book', 'book' ),
-    'edit_item' => _x( 'Edit book', 'book' ),
-    'new_item' => _x( 'New book', 'book' ),
-    'view_item' => _x( 'View book', 'book' ),
-    'search_items' => _x( 'Search Books', 'book' ),
-    'not_found' => _x( 'No books found', 'book' ),
-    'not_found_in_trash' => _x( 'No books found in Trash', 'book' ),
-    'parent_item_colon' => _x( 'Parent book:', 'book' ),
+    'edit_item' => _x( 'Edit cookbook', 'book' ),
+    'new_item' => _x( 'New cookbook', 'book' ),
+    'view_item' => _x( 'View cookbook', 'book' ),
+    'search_items' => _x( 'Search CookBooks', 'book' ),
+    'not_found' => _x( 'No cookbooks found', 'book' ),
+    'not_found_in_trash' => _x( 'No cookbooks found in Trash', 'book' ),
+    'parent_item_colon' => _x( 'Parent cookbook:', 'book' ),
     'menu_name' => _x( 'Books', 'book' ),
     );
     $args = array(
